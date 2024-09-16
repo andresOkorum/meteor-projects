41f3ef6908776e4227972e61e1c4bf08ef678e64
Andres Aguirre Giraldo <andresaguirregiraldo@Andress-MacBook-Air.local>
2024-09-16 17:13:49 -0500

    First meteor wallet version

---
+++ meteor-wallet/imports/ui/ContactList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { ContactsCollection } from '../api/ContactsCollection';
+
+export const ContactList = () => {
+
+  const contacts = useTracker(() => {
+    return ContactsCollection.find({}).fetch();
+  }); 
+
+  return (
+    <>
+      <h3>Contact List:</h3>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.email}>{contact.name} - {contact.email}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
