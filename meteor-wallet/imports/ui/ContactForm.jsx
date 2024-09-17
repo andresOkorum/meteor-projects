d6241bcea23acb073f1b73279dc0ea3efa6ba2eb
Andres Aguirre Giraldo <andresaguirregiraldo@Andress-MacBook-Air.local>
2024-09-17 13:40:11 -0500

    Add modal component

--- meteor-wallet/imports/ui/ContactForm.jsx
+++ meteor-wallet/imports/ui/ContactForm.jsx
@@ -7,6 +7,7 @@ export const ContactForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+  const [walletId, setWalletId] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
 
@@ -26,13 +27,14 @@ export const ContactForm = () => {
 
   const saveContact = () => {
     
-    Meteor.call('contacts.insert', { name, email, imageUrl }, (errorResponse) => {
+    Meteor.call('contacts.insert', { name, email, imageUrl, walletId }, (errorResponse) => {
       if (errorResponse) {
         showError({ message: errorResponse.error });
       } else {
         setName("");
         setEmail("");
         setImageUrl("");
+        setWalletId("");
         showSuccess({ message: "Contact saved!" });
       }
     });
@@ -78,6 +80,19 @@ export const ContactForm = () => {
             id="imageUrl"
             value={imageUrl}
             onChange={(e) => setImageUrl(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label htmlFor="walletId" className="block text-sm font-medium text-gray-700">
+            Wallet ID
+          </label>
+          <input
+            type="text"
+            id="walletId"
+            value={walletId}
+            onChange={(e) => setWalletId(e.target.value)}
             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
--- meteor-wallet/imports/ui/Wallet.jsx
+++ meteor-wallet/imports/ui/Wallet.jsx
@@ -1,39 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "./components/Modal";
 
-export const Wallet = ()  => {
+export const Wallet = () => {
+  const [open, setOpen] = useState(false);
+  const [isTransferring, setIsTransferring] = useState(false);
+  const [amount, setAmount] = useState("");
+  const [destinationWallet, setDestinationWallet] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   const wallet = {
     _id: '122312434232',
     balance: 5,
     currency: 'USD'
-  }
+  };
+
+  const addTransaction = () => {
+    console.log('New transaction!', amount, destinationWallet);
+  };
+
   return (
-    <div class="flex font-sans shadow-md my-10">
-      <form class="flex-auto p-6">
-        <div class="flex flex-wrap">
-          <div class="w-full flex-none text-sm font-medium text-gray-500">
+    <><div className="flex font-sans shadow-md my-10">
+      <form className="flex-auto p-6">
+        <div className="flex flex-wrap">
+          <div className="w-full flex-none text-sm font-medium text-gray-500">
             Main account
           </div>
-          <div class="w-full flex-none text-sm font-medium text-gray-500 mt-2">
+          <div className="w-full flex-none text-sm font-medium text-gray-500 mt-2">
             Wallet ID:
           </div>
-          <h1 class="flex-auto text-lg font-semibold text-gray-700">
+          <h1 className="flex-auto text-lg font-semibold text-gray-700">
             {wallet._id}
           </h1>
-          <div class="text-lg font-semibold text-gray-500">
+          <div className="text-lg font-semibold text-gray-500">
             {`${wallet.balance} ${wallet.currency}`}
           </div>
         </div>
-        <div class="flex space-x-4 text-sm font-medium">
-          <div class="flex-auto flex space-x-4 mt-4">
+        <div className="flex space-x-4 text-sm font-medium">
+          <div className="flex-auto flex space-x-4 mt-4">
             <button
               type="button"
               className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+              onClick={() => {
+                setIsTransferring(false);
+                setOpen(true);
+              }}
             >
               Add money
             </button>
             <button
               type="button"
               className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+              onClick={() => {
+                setIsTransferring(true);
+                setOpen(true);
+              }}
             >
               Transfer money
             </button>
@@ -41,5 +61,56 @@ export const Wallet = ()  => {
         </div>
       </form>
     </div>
+    
+    <Modal 
+      open={open}
+      setOpen={setOpen}
+      title={
+        isTransferring ? 'Transfer money to another wallet' : 'Add money to your wallet'
+      }
+      body={
+        <>
+          {isTransferring && (<div>
+            <label htmlFor="destinationWallet" className="block text-sm font-medium text-gray-700">
+              Destination Wallet
+            </label>
+            <input
+              type="string"
+              id="destinationWallet"
+              value={destinationWallet}
+              onChange={(e) => setDestinationWallet(e.target.value)}
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>)}
+
+          <div>
+            <label htmlFor="amount" className="block text-sm font-medium text-gray-700">
+              Amount
+            </label>
+            <input
+              type="number"
+              id="amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="0.00"
+            />
+          </div>
+        </>
+      }
+      footer={
+        <>
+          <button
+              type="button"
+              className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+              onClick={addTransaction}
+            >
+              {isTransferring ? 'Transfer' : 'Add'}
+            </button>
+        </>
+      }
+      errorMessage={errorMessage}
+    />
+    </>
   )
 }
