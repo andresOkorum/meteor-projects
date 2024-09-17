07718073413e73761688ec96cc1578d47411f5f5
Andres Aguirre Giraldo <andresaguirregiraldo@Andress-MacBook-Air.local>
2024-09-17 10:41:31 -0500

    Add wallet banner with account descriptio

--- meteor-wallet/server/main.js
+++ meteor-wallet/server/main.js
@@ -2,5 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/ContactsCollection';
 import '../imports/api/ContactsMethods';
 import '../imports/api/ContactsPublications';
+import '../imports/api/TransactionsCollection';
+import '../imports/api/WalletsCollection';
 
 Meteor.startup(async () => {});
