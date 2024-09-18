c8cd8f22c4b586720e344815c9f55a91a402eb6e
Andres Aguirre Giraldo <andresaguirregiraldo@Andress-MacBook-Air.local>
2024-09-18 11:43:04 -0500

    Update heroicons to v2

--- meteor-wallet/imports/ui/components/SelectContact.jsx
+++ meteor-wallet/imports/ui/components/SelectContact.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Listbox, Transition } from "@headlessui/react";
-import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+import CheckIcon from "@heroicons/react/24/solid/CheckIcon";
+import ChevronUpDownIcon from "@heroicons/react/24/solid/ChevronUpDownIcon";
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
@@ -29,7 +30,7 @@ export const SelectContact = ({ title, contact, setContact, contacts }) => {
                                 </span>
                             </span>
                             <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                <SelectorIcon
+                                <ChevronUpDownIcon
                                     className="h-5 w-5 text-gray-400"
                                     aria-hidden="true"
                                 />
