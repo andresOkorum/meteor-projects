2b850bef90287a05d247a38b6073e891bd1683ca
Andres Aguirre Giraldo <andresaguirregiraldo@Andress-MacBook-Air.local>
2024-09-19 15:08:18 -0500

    New admin

--- infra/roles.js
+++ infra/roles.js
@@ -5,7 +5,7 @@ import { WalletRoles } from './WalletRoles';
 Roles.createRole(WalletRoles.ADMIN, { unlessExists: true });
 
 Meteor.startup(() => {
-  const user = Meteor.users.findOne({ email: 'dse@eves.dsfe' });
+  const user = Meteor.users.findOne({ email: 'andres.aguirre@okorum.com' });
   if (!user || Roles.userIsInRole(user._id, WalletRoles.ADMIN)) {
     return;
   }
