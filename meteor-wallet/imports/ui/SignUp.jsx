9535364bcb96c0240ccbac5740d1feb33285efba
Andres Aguirre Giraldo <andresaguirregiraldo@Andress-MacBook-Air.local>
2024-09-19 11:06:04 -0500

    Google account authentication

--- meteor-wallet/imports/ui/SignUp.jsx
+++ meteor-wallet/imports/ui/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { useNavigate } from 'react-router-dom';
 import { RoutePaths } from './RoutePaths';
 import { Passwordless } from 'meteor/quave:accounts-passwordless-react';
@@ -10,9 +11,22 @@ export const SignUp = () => {
     navigate(RoutePaths.HOME);
   };
 
+  const loginWithGoogle = () => {
+    Meteor.loginWithGoogle({ loginStyle: 'redirect' });
+  };
+
   return (
-    <Passwordless
-      onEnterToken={onEnterToken}
-    />
+    <div className="flex flex-col items-center space-y-4">
+      <Passwordless
+        onEnterToken={onEnterToken}
+      />
+      <div className="font-light">or</div>
+      <button
+        className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        onClick={loginWithGoogle}
+      >
+        Login With Google
+      </button>
+    </div>
   );
 };
--- meteor-wallet/infra/accounts.js
+++ meteor-wallet/infra/accounts.js
@@ -1,18 +1,45 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
-import { RoutePaths } from '../imports/ui/RoutePaths';
+import { ServiceConfiguration } from 'meteor/service-configuration';
 import { WalletsCollection } from '../imports/api/collections/WalletsCollection';
 
+const getEmailFromUser = (user) => {
+  if (user.services?.google) {
+    return user.services.google.email;
+  }
+
+  return user.emails[0].address;
+};
+
 Accounts.onCreateUser((options, user) => {
-  const customizedUser = {...user};
+  const customizedUser = { ...user };
 
   WalletsCollection.insert({ userId: user._id, createdAt: new Date() });
 
-  customizedUser.email = user.emails[0].address;
+  customizedUser.email = getEmailFromUser(user);
   return customizedUser;
 });
 
 Accounts.setDefaultPublishFields({
   ...Accounts._defaultPublishFields.projection,
   email: 1,
+});
+
+const settings = Meteor.settings || {};
+Meteor.startup(() => {
+  if (!settings.googleClientId || !settings.googleSecret) {
+    throw new Error('Google required');
+  }
+
+  Accounts.config({ restrictCreationByEmailDomain: 'okorum.com' });
+  ServiceConfiguration.configurations.upsert({
+    service: 'google',
+  },
+  {
+    $set: {
+      service: 'google',
+      clientId: settings.googleClientId,
+      secret: settings.googleSecret,
+    },
+  });
 });
